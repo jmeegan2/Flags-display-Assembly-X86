1d2bbb04131c326b9f47d4c672efdc8b20f8bc84
James Meegan <jmeegan2@owlmail.harford.edu>
2021-03-29 00:24:02 -0400

    Real Progress made
    a breakthrough of sorts

--- Source.asm
+++ Source.asm
@@ -1,10 +1,6 @@
-
-;By: James Meegan 3/25/21
-
 ; EXTERNAL DEPENDENCIES
-
-INCLUDE Irvine32.inc
-INCLUDELIB Irvine32.lib
+INCLUDE		Irvine32.inc
+INCLUDELIB	Irvine32.lib
 
 ; EXECUTION MODE PARAMETERS
 .386
@@ -16,53 +12,145 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 ; DATA SEGMENT
 .data
-eFlagContents		BYTE ?
 
-; -- Status Flag Names --
-; NOTE: After a call to LAHF, AH is loaded with EFLAGS status flag
-;       information. AH = EFLAGS(SF:ZF:00:AF:00:PF:01:CF).  Notice
-;       that from the least significant position, bits 1, 3 and 5 are reserved.
-;       SF = Sign Flag				-- Set to 1 if the result of an immediate operation is negative.
-;       ZF = Zero Flag				-- Set to 1 if the result of an immediate operation is zero.
-;       AF = Auxillary Carry Flag	-- Set to 1 if the result of an immediate operation resulted in
-;									--  a carry bit moving from bit position 3 to bit position 4
-;									--  in the lowest BYTE.
-;       PF = Parity Flag			-- Set to 1 if the result of an immediate operation stored a value
-;									--  with an ODD number of 1 bits.  Set to 0 if the result of an
-;									--  immediate operation stored a value with an EVEN number of 1 bits.
-;       CF = Carry Flag				-- Set to 1 if a bit carried out of the most significant position
-;									--  on an unsigned integer encoding.
+ShowNewline		TEXTEQU	<CALL Crlf>
+
+cornerLeft					EQU		201d	; 201 is the base-10 ASCII code for open ended left corner
+theDoubleLinesSymbol		EQU		205d	; 205 is the base-10 ASCII code for "="
+tShapeConnector				EQU		203d	; 203 is the base-10 ASCII code for t shape open ended connector
+cornerRight					EQU		187d	; 187 is the base-10 ASCII code for open ended right corner 
+straightLinesDown			EQU		186d	; 186 is the base-10 ASCII code for straight double line down
+letterSalph					EQU		83d		; 83 is the base-10 ASCII code for a letter S
+letterFalph					EQU		70d		; 70 is the base-10 ASCII code for a letter F
+
+
+
+
+; --- DUP Operator ---
+; The DUP operator can be used to replicate symbolic constant text an arbitrary number of times.
+; DUP is commonly used to initialize large data segment arrays.
+;arraySizeMessage	BYTE	"'howeverManyAs' array size currently set to: ", 0
+
+cornerL			BYTE	1	DUP(cornerLeft), 0
+flatLines		BYTE	3 DUP(theDoubleLinesSymbol), 0
+tConnector		BYTE	1 DUP(tShapeConnector),0
+cornerR			BYTE	1 DUP(cornerRight), 0
+straightLineD	BYTE    1 DUP(straightLinesDown), 0
+letterS			BYTE	1 DUP(letterSalph), 0
+letterF			BYTE	1 DUP(letterFalph), 0
+
+
+
+;
+;howeverManyAs		BYTE	arraySize DUP("A"), 0
+
+; --- CURRENT LOCATION COUNTER ($) ---
+; The current location is the 32-bit memory address associated with the current offset
+; into the code segment.  This value can be casually retrieved with the '$' symbol used
+; in place of a 32-bit integer literal (memory addresses take 
+; the form of DWORDs, or 32-bit on x86).
+;endOfAsOffset		DWORD	$
+
+; NOTE: By capturing the offset address at 'endOfAsOffset', we can calculate the size
+; of 'howeverManyAs' by creating a literal symbolic constant containing the difference
+; between the two addresses.  Since this is an array of BYTE variables, a single offset
+; address correlates exactly to a single element in the string.  Keep in mind the null
+; byte was also appened to the end of the string to ensure compatibility with display
+; functions.  This null byte also takes up a BYTE when the size calculation takes place.
 
 ; CODE SEGMENT
 .code
 main PROC
-
-
-	; Clobber EAX with all 0s and show the clean slate.
-	MOV EAX, 0
 	
-	CALL Crlf
+	MOV EDX, OFFSET cornerL
+	CALL WriteString
 	
-	; "lahf" - load flag data into AH - This is a single command that appears alone.
-	;                                   when the command is executed, the lower BYTE (8-bits)
-	;                                   of the EFLAGS register is loaded directly to AH.
-	lahf
-	MOV eFlagContents, AH				; Store a copy of the flag contents for future use.
+	; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
 
-	; Now that the flag data has been captured in 'eFlagContents' (8-bits), we must copy in
-	;  into EAX (32-bits).
-	MOVZX EAX, eFlagContents
-
-
-	; Show the contents of AL (the place where the flag data should have been copied at this point).
-	MOV EBX, TYPE BYTE
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
 	
-	CALL WriteBinB ;Intial flag contents displayed here
-	CALL Crlf
+	; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
 
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+	
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
 
-	; Return to OS.
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
+
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
+
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
+
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
+
+	MOV EDX, OFFSET tConnector
+	CALL WriteString 
+
+		; Show the string with 5 = signs.
+	MOV EDX, OFFSET flatLines
+	CALL WriteString
+
+	MOV EDX, OFFSET cornerR	
+	CALL WriteString 
+	ShowNewline
+
+	MOV EDX, OFFSET straightLineD
+	CALL WriteString
+
+	MOV EDX, OFFSET letterS
+	call WriteString 
+	
+	MOV EDX, OFFSET letterF
+	call WriteString 
+
+	; Show 'arraySize' As.
+	;MOV EDX, OFFSET howeverManyAs
+	;CALL WriteString
+	;ShowNewline
+
+	; Establish and display 'howeverManyAs' array size by adjusting the constant
+	; difference between 'endOfAsOffset' and 'howeverManyAs'.
+	;MOV EDX, OFFSET arraySizeMessage
+	;CALL WriteString
+	;MOV EAX, (endOfAsOffset - howeverManyAs)		; it is arraySize + 1 since the null byte counts
+	;DEC EAX											; adjust for null byte
+	; NOTE: We were only able to perform (endOfAsOffset - howeverManyAs) as a literal expression
+	; subtraction because at this point in the code, both identifiers resolve to constant (immediate)
+	; values.  If either of these values was associated with a memory location for storate, this
+	; different would have to have been calculated through a CPU operation (probably SUB).
+	;CALL WriteDec
+	ShowNewline
+	
+	; return to OS
 	INVOKE ExitProcess, 0
 main ENDP
 
-END main								; end of OPCODES
+END main				; end of program OPCODES
